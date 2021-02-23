dd7627086d84eaf40bb7935daef126abd74fd1d0
magaligarcia <magalimaribelgarcia@gmail.com>
2021-02-23 14:09:09 -0300

    Resuelto

--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -2,117 +2,37 @@
 MAGALI GARCIA DIV H
 Al presionar el botón pedir  números  hasta que el usuario quiera,
 mostrar el número máximo y el número mínimo ingresado.
-9 bis( se pide el menor de los pares y el mayor de los negativos)
 */
-
-
+/*
+Al presionar el botón pedir  números  hasta que el usuario quiera,
+mostrar el número máximo y el número mínimo ingresado.*/
 function mostrar()
-{
-var banderaDelPrimero;
-var numeroIngresado;
-var numeroMaximo;
-var numeroMinimo;
-var menorDeLosPares;
-var mayorDeLosNegativos;
-var respuesta;
-
-banderaDelPrimero="es el primero";
-mayorDeLosNegativos=-10000;
-respuesta='si';
-
-while( respuesta == "si")
-{
-	numeroIngresado = prompt('Ingrese un número');
-	numeroIngresado = parseInt(numeroIngresado);
-
-	if(banderaDelPrimero == "es el primero" || numeroIngresado<numeroMinimo)
+{	// declarar variables
+	var banderaDelPrimero;
+	var numeroIngresado;
+	var numeroMaximo;
+	var numeroMinimo;
+	var respuesta;
+	//iniciar variables
+	banderaDelPrimero="es el primero";
+	respuesta='si';
+	while(respuesta=="si")
+	{
+		numeroIngresado = prompt('Ingrese un número');
+		numeroIngresado = parseInt(numeroIngresado);
+		
+		if(banderaDelPrimero == "es el primero" || numeroIngresado<numeroMinimo)
 		{
 			numeroMinimo = numeroIngresado;
-			if(numeroMinimo % 2 == 0){
-				menorDeLosPares = numeroMinimo;
-			}
 		}
-	if(numeroIngresado < 0 && numeroIngresado > mayorDeLosNegativos)
-	{
-		mayorDeLosNegativos = numeroIngresado;
-	}
+		if(banderaDelPrimero == "es el primero" || numeroIngresado>numeroMaximo)
+		{
+			numeroMaximo = numeroIngresado;
+			banderaDelPrimero = "No es el primer número";			
+		}
 
-	if(banderaDelPrimero == "es el primero" || numeroIngresado>numeroMaximo)
-	{
-		numeroMaximo = numeroIngresado;
-		banderaDelPrimero = "No es el primer número";			
+		respuesta=prompt("desea continuar? Si/No").toLowerCase();
 	}
-	respuesta=prompt("desea continuar? Si/No").toLowerCase();
-}
-
 	txtIdMaximo.value=numeroMaximo;
 	txtIdMinimo.value=numeroMinimo;
-	alert("El menor de los pares es: " + menorDeLosPares + " y el mayor de los negativos es: " + mayorDeLosNegativos);
-
-}//FIN DE LA FUNCION
-
-
-// function mostrar()
-// {
-// 	var banderaDelPrimero;
-// 	var numeroIngresado;
-// 	var numeroMaximo;
-// 	var numeroMinimo;
-// 	var numeroPar;
-// 	var menorDeLosPares;
-// 	var mayorDeLosNegativos;
-// 	var respuesta;
-
-// 	banderaDelPrimero="es el primero";
-// 	mayorDeLosNegativos="es el primero";
-// 	respuesta='si';
-// 	while( respuesta == "si")
-// 	{
-// 		numeroIngresado = prompt('Ingrese un número');
-// 		numeroIngresado = parseInt(numeroIngresado);
-	
-// 		if(numeroIngresado % 2 == 0)
-// 		{
-// 			numeroPar = numeroIngresado;
-// 			if(numeroPar < menorDeLosPares){
-// 				menorDeLosPares = numeroIngresado
-// 			}
-// 		}
-// 	respuesta=prompt("desea continuar? Si/No").toLowerCase();
-// 	}
-
-// }//FIN DE LA FUNCION
-
-// /*
-// Al presionar el botón pedir  números  hasta que el usuario quiera,
-// mostrar el número máximo y el número mínimo ingresado.*/
-// function mostrar()
-// {	// declarar variables
-// 	var banderaDelPrimero;
-// 	var numeroIngresado;
-// 	var numeroMaximo;
-// 	var numeroMinimo;
-// 	var respuesta;
-// 	//iniciar variables
-// 	banderaDelPrimero="es el primero";
-// 	respuesta='si';
-// 	while(respuesta=="si")
-// 	{
-// 		numeroIngresado = prompt('Ingrese un número');
-// 		numeroIngresado = parseInt(numeroIngresado);
-		
-// 		if(banderaDelPrimero == "es el primero" || numeroIngresado<numeroMinimo)
-// 		{
-// 			numeroMinimo = numeroIngresado;
-// 		}
-// 		if(banderaDelPrimero == "es el primero" || numeroIngresado>numeroMaximo)
-// 		{
-// 			numeroMaximo = numeroIngresado;
-// 			banderaDelPrimero = "No es el primer número";			
-// 		}
-
-// 		respuesta=prompt("desea continuar? Si/No").toLowerCase();
-// 	}
-// 	txtIdMaximo.value=numeroMaximo;
-// 	txtIdMinimo.value=numeroMinimo;
-// }//FIN DE LA FUNCIÓN
+}//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteracionesWhile-10.js
+++ 5-InstruccionWhile/jsIteracionesWhile-10.js
@@ -15,17 +15,63 @@ function mostrar()
 	//declarar contadores y variables 
 	var respuesta;
 	var numeroIngresado;
-	var sumaNegativos=0;
+	var acumuladorNegativos;
+	var contadorNegativos;
+	var acumuladorPositivos;
+	var contadorPositivos;
+	var contadorCeros;
+	var contadorPares;
+	var promedioPositivos;
+	var promedioNegativos;
+	var diferenciaPositivosNegativos;
 
 	respuesta="si";
+	acumuladorNegativos=0;
+	acumuladorPositivos=0;
+	contadorNegativos=0;
+	contadorPositivos=0;
+	contadorCeros=0;
+	contadorPares=0;
 
 	while(respuesta=="si")
 	{
 		numeroIngresado = prompt("Ingrese un número");
 		numeroIngresado = parseInt(numeroIngresado);
 		
+		if(numeroIngresado > 0)
+		{
+			acumuladorPositivos = acumuladorPositivos + numeroIngresado;
+			contadorPositivos++;
+		} else {
+			if(numeroIngresado < 0){
+				acumuladorNegativos = acumuladorNegativos + numeroIngresado;
+				contadorNegativos++;
+			} else
+			contadorCeros++;
+			}
+		if(numeroIngresado % 2 == 0)
+		{
+			contadorPares++;
+		}
+		
 		respuesta=prompt("desea continuar? Si/No").toLowerCase();
+		while(respuesta != "si" && respuesta != "no"){
+			respuesta=prompt("ERROR. Desea continuar ingresando números? Responda SI o NO: ").toLowerCase();
+		}
+		
 	}//fin del while
+	promedioPositivos = acumuladorPositivos / contadorPositivos;
+	promedioNegativos = acumuladorNegativos / contadorNegativos;
+	diferenciaPositivosNegativos = acumuladorPositivos - acumuladorNegativos;
 
-	document.write("la suma de negativos es :"+sumaNegativos);
+	document.write(
+	". La suma de los negativos es: "+acumuladorNegativos+
+	". La suma de los positivos es: "+acumuladorPositivos+
+	". La cantidad de positivos es: "+contadorPositivos+
+	". La cantidad de negativos es: "+contadorNegativos+
+	". La cantidad de ceros es: "+contadorCeros+
+	". La cantidad de números pares es: "+contadorPares+
+	". El promedio de positivos es: "+promedioPositivos+
+	". El promedios de negativos es: "+promedioNegativos+
+	". La diferencia entre positivos y negativos es: "+diferenciaPositivosNegativos);
 }//FIN DE LA FUNCIÓN
