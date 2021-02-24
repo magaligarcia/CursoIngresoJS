ebe5329fba689cfd3d632860fb683e1d55773ad2
magaligarcia <magalimaribelgarcia@gmail.com>
2021-02-24 08:56:45 -0300

    Actualizado

--- 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
@@ -1,13 +1,14 @@
-// Debemos realizar la carga de 5(cinco) productos de prevención de contagio, 
-// 			de cada una debo obtener los siguientes datos: 
-// 			el tipo (validar "barbijo" , "jabón" o "alcohol") , 
-// 			el precio (validar entre 100 y 300),
-// 			la cantidad de unidades (no puede ser 0 o negativo y no debe superar las 1000 unidades), 
-// 			 la Marca y el fabricante. 
-// 			Se debe Informar al usuario lo siguiente:
-// 			a) Del más barato de los alcohol, la cantidad de unidades y el fabricante
-// 			b) Del tipo con mas unidades, el promedio por compra 
-// 			c) Cuántas unidades de jabones hay en total
+/*  Magali Garcia - Div H - EJERCICIO UNO, PARCIAL 2020
+    
+    Debemos realizar la carga de 5(cinco) productos de prevención de contagio, 
+    de cada una debo obtener los siguientes datos: 
+    el tipo (validar "barbijo" , "jabón" o "alcohol") , 
+    el precio (validar entre 100 y 300),
+    la cantidad de unidades (no puede ser 0 o negativo y no debe superar las 1000 unidades), la Marca y el fabricante. 
+    Se debe Informar al usuario lo siguiente:
+        a) Del más barato de los alcohol, la cantidad de unidades y el fabricante
+        b) Del tipo con mas unidades, el promedio por compra 
+        c) Cuántas unidades de jabones hay en total */
 function mostrar()
 {
 var tipoProducto;
@@ -21,7 +22,7 @@ var precioAlcoholMasBarato;
 var cantidadAlcoholMasBarato;
 var fabricanteAlcoholMasBarato;
 var flagMayorCantidad;
-var tipoMayorCantidad;
+var flagTipoMayorCantidad;
 var promedioDelTipoConMasUnidades;
 var contadorJabones;
 
@@ -34,16 +35,38 @@ flagMayorCantidad=0;
 while(contadorProductos<5)
 {
     tipoProducto = prompt("Ingrese el tipo de producto: BARBIJO - JABÓN O ALCOHOL ").toLowerCase();
-    while(tipoProducto != "barbijo" && tipoProducto != "jabon" && tipoProducto != "alcohol")
+    while(isNaN(tipoProducto)==false || tipoProducto != "barbijo" && tipoProducto != "jabon" && tipoProducto != "alcohol") //VALIDACION DE TIPO
     {
         tipoProducto = prompt("ERROR! Ingrese el tipo de producto: BARBIJO - JABÓN O ALCOHOL ").toLowerCase();
     }
+
     precio = prompt("Ingrese el precio (entre 100 y 300)");
     precio = parseInt(precio);
+    while(isNaN(precio)==true || precio < 100 || precio > 300)//VALIDACION DE PRECIO 
+    {
+        precio = prompt("ERROR. El precio debe ser entre 100 y 300:");
+        precio = parseInt(precio);
+    }
+
     cantidad = prompt("Ingrese la cantidad de unidades entre 1 y 1000:");
     cantidad = parseInt(cantidad);
+    while(isNaN(cantidad)==true || cantidad < 1 || cantidad > 1000)
+    {
+        cantidad = prompt("ERROR. La cantidad de unidades debe ser entre 1 y 1000:");
+        cantidad = parseInt(cantidad);
+    }
+
     marca = prompt("Ingrese la marca del producto:");
+    while(isNaN(marca)== false)
+    {
+        marca = prompt("ERROR. Reingrese la marca del producto:");
+    }
+
     fabricante = prompt("Ingrese el nombre del fabricante: ");
+    while(isNaN(fabricante)== false)
+    {
+        fabricante = prompt("ERROR. Reingrese el nombre del fabricante:");
+    }
 
     if(tipoProducto == "alcohol")
     {
@@ -54,7 +77,7 @@ while(contadorProductos<5)
             fabricanteAlcoholMasBarato = fabricante;
             flagPrimerAlcohol="NO es el primero";
         } else {
-            if(precio < precioAlcoholMasBarato) // COMPARO PRECIOS DE ALCOHOL
+            if(precio < precioAlcoholMasBarato) // SI NO ES EL PRIMERO, COMPARO PRECIOS
             {
                 precioAlcoholMasBarato = precio;
                 cantidadAlcoholMasBarato = cantidad;
@@ -63,21 +86,25 @@ while(contadorProductos<5)
         }
     } else 
     {
-        if(tipoProducto == "jabon")
+        if(tipoProducto == "jabon") // CANTIDAD TOTAL DE JABONES
         {
             contadorJabones = contadorJabones + cantidad;
         }
     }
-    if(cantidad>flagMayorCantidad)
+
+    if(cantidad>flagMayorCantidad) // Del tipo con mas unidades
     {
         flagMayorCantidad=cantidad;
-        tipoMayorCantidad=tipoProducto;
+        flagTipoMayorCantidad=tipoProducto;
     }
     
-
 contadorProductos++;
 }//FIN WHILE 5 UNIDADES
-promedioDelTipoConMasUnidades = 
-document.write("Del más barato de los alcohol, la cantidad de unidades es: " +cantidadAlcoholMasBarato+  " y el fabricante es: "+fabricanteAlcoholMasBarato+ " La cantidad total de jabones es: " +contadorJabones);
+
+promedioDelTipoConMasUnidades = flagMayorCantidad / 5; //el promedio por compra 
+
+document.write("Del más barato de los alcohol, la cantidad de unidades es: "+cantidadAlcoholMasBarato+ " y el fabricante es: "+fabricanteAlcoholMasBarato+"<br>");
+document.write("El promedio del tipo con más unidades: "+ +"<br>");
+document.write("La cantidad total de jabones es: " +contadorJabones);
 
 }//FIN FUNCION
